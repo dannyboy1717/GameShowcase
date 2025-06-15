c0a5317b64da615a078ac58f722436dc7829f821
Daniel Hughes <dannyboy1717@hotmail.co.uk>
2025-06-15 17:45:16 +0100

    Set up project for expo and tailwind. Set up basic layout

--- GameShowcase/app/(tabs)/_layout.tsx
+++ GameShowcase/app/(tabs)/_layout.tsx
@@ -2,11 +2,11 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { HapticTab } from '../components/HapticTab';
+import { IconSymbol } from '../components/ui/IconSymbol';
+import TabBarBackground from '../components/ui/TabBarBackground';
+import { Colors } from '../constants/Colors';
+import { useColorScheme } from '../hooks/useColorScheme';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -27,16 +27,16 @@ export default function TabLayout() {
         }),
       }}>
       <Tabs.Screen
-        name="index"
+        name="games"
         options={{
-          title: 'Home',
+          title: 'View',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="add-games"
         options={{
-          title: 'Explore',
+          title: 'Add',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
---
+++ GameShowcase/app/(tabs)/add-games.tsx
@@ -0,0 +1,10 @@
+import { Stack } from "expo-router";
+import React from "react";
+import { Text } from "react-native";
+
+export default function Games() {
+
+  return (
+    <></>
+  );
+}
